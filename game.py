b2420aeed351f078b84569f1d0d080fbef0bc335
drabiot <tchartie@student.42angouleme.fr>
2026-09-09 20:10:34 +0200

    Add Snake identity

--- game.py
+++ game.py
@@ -4,8 +4,9 @@ import random
 
 SCREEN_WIDTH	= 800
 SCREEN_HEIGHT	= 800
-TILE_WIDTH		= 50
-TILE_HEIGHT		= 50
+TILE_WIDTH		= 10
+TILE_HEIGHT		= 10
+FPS				= 7
 
 class Snake(arcade.Window):
 	"""
@@ -18,17 +19,20 @@ class Snake(arcade.Window):
 		self.all_sprites = arcade.SpriteList()
 
 		# Set Framerate
-		self.set_update_rate(1 / 10)
+		self.set_update_rate(1 / FPS)
 
-		# Set Moveement
+		# Set Movement
 		self.up = self.down = self.left = self.right = False
+		self.start = False
 
 		# Create Player texture
-		self.snake_texture = arcade.make_soft_square_texture(int(SCREEN_WIDTH / TILE_WIDTH), arcade.color.LIME, 255, 255)
+		self.snake_texture_even = arcade.make_soft_square_texture(int(SCREEN_WIDTH / TILE_WIDTH), arcade.color.BITTER_LIME, 255, 255)
+		self.snake_texture_odd = arcade.make_soft_square_texture(int(SCREEN_WIDTH / TILE_WIDTH), arcade.color.BITTER_LEMON, 255, 255)
+		self.snake_head_texture = arcade.make_soft_square_texture(int(SCREEN_WIDTH / TILE_WIDTH), arcade.color.RED, 255, 255)
 
 		# Generate Player Coordinate
-		base_pos_x = random.randint(1, TILE_WIDTH)	#Set to 0
-		base_pos_y = random.randint(1, TILE_HEIGHT)	#Set to 0
+		base_pos_x = random.randint(1, TILE_WIDTH)
+		base_pos_y = random.randint(1, TILE_HEIGHT)
 
 		if (random.randint(0, 1)):
 			base_dir_x = 0
@@ -46,7 +50,7 @@ class Snake(arcade.Window):
     		(base_pos_x - base_dir_x, base_pos_y - base_dir_y), 
     		(base_pos_x - (base_dir_x * 2), base_pos_y - (base_dir_y * 2))
 		] #Create correctly the body (check if part outside the wall)
-		self.direction = (base_dir_x, base_dir_y)
+		self.direction = (0, 0)
 		self.grow = False
 
 	def move(self):
@@ -80,7 +84,8 @@ class Snake(arcade.Window):
 		if (self.right and self.direction != (-1, 0)):
 			self.direction = (1, 0)
 
-		self.move()
+		if (self.start and not self.move()):
+			arcade.close_window()
 
 	def on_draw(self):
 		"""
@@ -92,12 +97,17 @@ class Snake(arcade.Window):
 		self.all_sprites.clear()
 
 		# Render each segment of the snake
-		for pos_x, pos_y in self.positions:
+		for i, (pos_x, pos_y) in enumerate(self.positions):
 			center_x = pos_x * (SCREEN_WIDTH / TILE_WIDTH) - ((SCREEN_WIDTH / TILE_WIDTH) / 2)
 			center_y = pos_y * (SCREEN_HEIGHT / TILE_HEIGHT) - ((SCREEN_HEIGHT / TILE_HEIGHT) / 2)
 
 			player = arcade.Sprite(center_x=center_x, center_y=center_y)
-			player.append_texture(self.snake_texture)
+			if (i == 0):
+				player.append_texture(self.snake_head_texture)
+			elif (not i % 2):
+				player.append_texture(self.snake_texture_even)
+			else:
+				player.append_texture(self.snake_texture_odd)
 			player.set_texture(0)
 			self.all_sprites.append(player)
 
@@ -107,6 +117,8 @@ class Snake(arcade.Window):
 		"""
 		Set movement on key press
 		"""
+		if (not self.start and (key == arcade.key.UP or key == arcade.key.DOWN or key == arcade.key.LEFT or key == arcade.key.RIGHT)):
+			self.start = True
 		if (key == arcade.key.UP):
 			self.up = True
 		elif (key == arcade.key.DOWN):
